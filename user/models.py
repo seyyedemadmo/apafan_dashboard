930e3150f308e4c2361d04a503f345b1616c72dd
seyyedemadmo <seyyedemadmo@gmail.com>
2023-02-07 15:04:18 +0330

    add user mmodel to project

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    is_band = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expire_time = models.DateTimeField(null=True, blank=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['username', 'password']
