e8010a9eb7934fb353a378d743275afdc49384d3
Emad <seyyedemadmo@gmail.com>
2023-03-11 12:41:47 +0330

    fix some bugs

--- mqtt/aysinc_functions/functions.py
+++ mqtt/aysinc_functions/functions.py
@@ -8,7 +8,8 @@ def start_mqtt_data_listener():
     mqtt = Mqtt(getattr(settings, "MQTT_ADDRESS", None),
                 getattr(settings, "MQTT_PORT", None),
                 username=getattr(settings, "MQTT_USER", None),
-                password=getattr(settings, "MQTT_PASSWORD", None)
+                password=getattr(settings, "MQTT_PASSWORD", None),
+                client_id="data_listener"
                 )
     mqtt.listen(getattr(settings, "BASE_MQTT_SUBSCRIBE_TOPIC", None))
     mqtt.run()
@@ -18,7 +19,8 @@ def start_mqtt_first_up_listener():
     mqtt = Mqtt(getattr(settings, "MQTT_ADDRESS", None),
                 getattr(settings, "MQTT_PORT", None),
                 username=getattr(settings, "MQTT_USER", None),
-                password=getattr(settings, "MQTT_PASSWORD", None)
+                password=getattr(settings, "MQTT_PASSWORD", None),
+                client_id="temp_device_listener"
                 )
     mqtt.listen(getattr(settings, "MQTT_TEMP_TOPIC", None))
     mqtt.massage_function = send_data_parameter_on_message
@@ -31,7 +33,8 @@ def start_mqtt_parameter_listener():
     client = Mqtt(getattr(settings, "MQTT_ADDRESS", None),
                   getattr(settings, "MQTT_PORT", None),
                   username=getattr(settings, "MQTT_USER", None),
-                  password=getattr(settings, "MQTT_PASSWORD", None)
+                  password=getattr(settings, "MQTT_PASSWORD", None),
+                  client_id="device_parameter_listener"
                   )
     print(getattr(settings, "MQTT_DEVICE_PARAMETER_SEND_TOPIC", None))
     client.listen(getattr(settings, "MQTT_DEVICE_PARAMETER_SEND_TOPIC", None))
@@ -44,7 +47,8 @@ def start_mqtt_head_parameter_listener():
     client = Mqtt(getattr(settings, "MQTT_ADDRESS", None),
                   getattr(settings, "MQTT_PORT", None),
                   username=getattr(settings, "MQTT_USER", None),
-                  password=getattr(settings, "MQTT_PASSWORD", None)
+                  password=getattr(settings, "MQTT_PASSWORD", None),
+                  client_id="head_parameter_listener"
                   )
     print(getattr(settings, "MQTT_HEAD_PARAMETER_SEND_TOPIC", None))
     client.listen(getattr(settings, "MQTT_HEAD_PARAMETER_SEND_TOPIC", None))
