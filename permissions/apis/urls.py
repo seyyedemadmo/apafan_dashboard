cafb14f3e39d53393bb11c161281e2353f85ea53
root <seyyedemadmo@gmail.com>
2023-03-18 12:53:24 +0330

    add api for add permissions

--- permissions/apis/urls.py
+++ permissions/apis/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.GetUserPermissions.as_view({"get": "list"}), name='get all user permission')
+    path('', views.GetUserPermissions.as_view({"get": "list"}), name='get all user permission'),
+    path("<str:username>/add", views.AddUserPermissionView.as_view(), name="add_user_perm"),
+    path("<str:username>/remove", views.RemoveUserPermissionView.as_view(), name='remove_user_perm'),
 ]
