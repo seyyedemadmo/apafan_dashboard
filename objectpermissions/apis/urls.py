ec6dd8790032a8967a2dedf3dabfc47b41e20dc8
root <seyyedemadmo@gmail.com>
2023-04-15 10:55:20 +0330

    fix some bug

--- objectpermissions/apis/urls.py
+++ objectpermissions/apis/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path("<str:model_name>/<str:username>/add", views.AddObjectPermissionView.as_view(), name='add_object_perm'),
     path("<str:model_name>/<str:username>/remmove", views.AddObjectPermissionView.as_view(), name='remove_object_perm'),
-
 ]
