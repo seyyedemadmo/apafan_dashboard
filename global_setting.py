77f891177fa633ed1f9ee19c3d49590a67f72158
Emad <seyyedemadmo@gmail.com>
2023-05-06 16:33:28 +0330

    remove aditional file

--- global_setting.py
+++
@@ -1,9 +0,0 @@
-global_setting = {
-    "time_expire": 2,
-    "expire_when_close": False,
-    "default_theme": "light",
-    "allow_host": "*",
-    "default_paginate_size": "50",
-    "swagger_enable": True,
-    "swagger_url": "api/doc/"
-}
