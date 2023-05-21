0349367ee99f061f8add06a741a72970d38fc0ad
Emad <seyyedemadmo@gmail.com>
2023-05-21 18:01:09 +0330

    complete and move for test chart websocket

--- chart/events/consumers.py
+++ chart/events/consumers.py
@@ -1,45 +1,53 @@
 # consumers.py
-
-import asyncio
+import json
 import time
-
-from asgiref.sync import sync_to_async
-from channels.generic.websocket import AsyncWebsocketConsumer
-from django.contrib.auth import get_user_model
+from threading import Thread, Event
+from channels.generic.websocket import WebsocketConsumer
 from django.shortcuts import get_object_or_404
 
+from hall.models import Device
 
-class ChartWebSocketConsumer(AsyncWebsocketConsumer):
 
-    async def connect(self):
+class ChartWebSocketConsumer(WebsocketConsumer):
+
+    def __init__(self, ):
+        super(ChartWebSocketConsumer, self).__init__(self)
+        self.data = {}
+        self.event = Event()
+        self.device = None
+        self.send_data_thread = Thread(target=self.send_data)
+
+    def connect(self):
         if self.scope['user'].is_authenticated:
-            await self.accept()
+            self.accept()
+            self.send_data_thread.start()
         else:
-            await self.close()
+            self.close()
 
-    async def receive(self, text_data=None, bytes_data=None):
-        if self.scope['client']:
-            # Send data when the client connects
-            try:
-                # Keep sending data until the client disconnects
-                while True:
-                    # Check if the client is still connected
-                    if not self.scope['client']:
-                        break
+    def receive(self, text_data=None, bytes_data=None):
+        try:
+            self.data = json.loads(text_data)
+            self.device = get_object_or_404(Device, chip_ip=self.data['chip_ip'])
 
-                    # Send your desired data to the client
-                    data = "Hello, client!"
-                    print(self.scope['client'])
-                    await self.send(data)
+        except Exception as e:
+            print(e.__str__())
 
-                    # Wait for a while before sending the next data
-                    time.sleep(1)
-            except asyncio.CancelledError:
-                # Handle the client disconnection
-                print("Client disconnected")
-
-    async def disconnect(self, code):
+    def disconnect(self, code):
         if self.scope['user'].is_authenticated:
             self.scope['user'].update_uuid()
         self.scope['client'] = None
-        await self.close()
+        self.event.set()
+        self.close()
+
+    def send_data(self):
+        while True:
+            if self.event.is_set():
+                break
+            self.send(json.dumps(self.get_data_now()))
+            time.sleep(1)
+
+    def get_data_now(self):
+        if self.device:
+            device_last_data_send = self.device.headdata_set.latest("receive_at").data
+            return {key: value for key, value in device_last_data_send.items() if key in self.data['fields']}
+        return {}
